c0eb083b7eef260f721c1d50a55a89f80109371b
Joseph Bermudez <106097275+Reiimerich@users.noreply.github.com>
2022-10-06 04:56:09 -0500

    Change the Rectangle for a Sprite

--- runner.cpp
+++ runner.cpp
@@ -5,13 +5,10 @@ int main()
     //Window Dimensions
     const int WindowHeight{600};
     const int WindowWidth{900};
-
-    //Rectangle Dimensions
-    const int width{50};
-    const int height{80};
+    //Create Window
+    InitWindow(WindowWidth,WindowHeight,"Runner Game");
 
     //Player Movement
-    int InitialPositionY{WindowHeight - height};
     int Velocity{0};
 
     //Physics
@@ -19,16 +16,21 @@ int main()
     bool IsJumping{};
 
     //Sprites
-    Texture2D Player = LoadTexture("textures/monster.png");
+    Texture2D Player = LoadTexture("textures/scarfy.png");
     Rectangle PlayerRec;
+    PlayerRec.width = Player.width/6;
+    PlayerRec.height = Player.height;
+    PlayerRec.x = 0;
+    PlayerRec.y = 0;
+
+    //Sprite position
     Vector2 PlayerPos;
+    PlayerPos.x = WindowWidth/2 - PlayerRec.width/2;
+    PlayerPos.y = WindowHeight - PlayerRec.height;
 
     //Jump Height
     const int JumpHeight{20};
 
-    //Create Window
-    InitWindow(WindowWidth,WindowHeight,"Runner Game");
-    
     SetTargetFPS(60);
     while (!WindowShouldClose())
     {
@@ -37,7 +39,7 @@ int main()
         ClearBackground(BLUE);
 
         //Apply gravity
-        if(InitialPositionY >= WindowHeight - height)
+        if(PlayerPos.y >= WindowHeight - PlayerRec.height)
         {
             Velocity = 0;
             IsJumping = false;
@@ -48,8 +50,8 @@ int main()
             IsJumping = true;
         }
 
-        //Draw Rectangle
-        DrawRectangle(WindowWidth/2, InitialPositionY, width,height,RED);
+        //Draw Player
+        DrawTextureRec(Player, PlayerRec, PlayerPos, WHITE);
 
         if(IsKeyPressed(KEY_SPACE) && !IsJumping)
         {
@@ -57,10 +59,11 @@ int main()
         }
 
         //Update X position
-        InitialPositionY += Velocity;
+        PlayerPos.y += Velocity;
 
         //End Drawing
         EndDrawing();
     }
+    UnloadTexture(Player);
     CloseWindow();
 }
